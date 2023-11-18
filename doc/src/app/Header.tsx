d5bcdd8e2eebba3fe7568e196357abbc6897a393
Naoya Yamashita <conao3@gmail.com>
2023-11-19 03:04:10 +0900

    add header

---
+++ doc/src/app/Header.tsx
@@ -0,0 +1,14 @@
+import { Flex, View } from "@adobe/react-spectrum";
+
+export function Header() {
+  return (
+    <Flex gap="size-200" minHeight="size-600">
+      <View>
+        Header1
+      </View>
+      <View>
+        Header2
+      </View>
+    </Flex>
+  )
+}
--- doc/src/app/page.tsx
+++ doc/src/app/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { Flex, Heading, View } from "@adobe/react-spectrum";
+import { Header } from "./Header";
+
 export default function Home() {
   return (
-    <main style={{minHeight: '100vh'}}>
-      Hello
-    </main>
+    <View minHeight="100vh" paddingTop="size-200">
+      <Flex direction="column" maxWidth="1000px" margin="0 auto">
+        <Header />
+        <Heading level={1} UNSAFE_style={{fontSize: "3em", textAlign: "center"}}>
+          A Brand New GraphQL Library Based on Pydantic
+        </Heading>
+      </Flex>
+    </View>
   )
 }
